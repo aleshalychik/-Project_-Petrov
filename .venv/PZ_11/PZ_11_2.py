b83990a1430c575f73821ea4a9cd3615e6472ee7
aleshalychik <fhhg561@gmail.com>
2026-04-17 17:47:01 +0300

    Update PZ_11_2.py

--- .venv/PZ_11/PZ_11_2.py
+++ .venv/PZ_11/PZ_11_2.py
@@ -1,15 +1,11 @@
 #Составить генератор (yield), который преобразует все буквенные символы в заглавные.
 
-def to_upper_gen(text):
+def upper(text):
     for char in text:
-        if char.isalpha():   # если это буква
+        if char.isalpha():
             yield char.upper()
         else:
-            yield char       # остальные символы без изменений
-
-
-# пример использования
-s = "Hello, мир 123"
-result = to_upper_gen(s)
-
-print("".join(result))
+            yield char
+s = "абвгдеежзийклмнопрстуфхцчшщъыьэюя"
+result = upper(s)
+print(list(map(lambda x: x, result)))
